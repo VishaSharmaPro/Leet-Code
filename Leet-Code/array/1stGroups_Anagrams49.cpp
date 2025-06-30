fa98c9abdb98208edf1e784f4696ada0465e8ef1
VishaSharmaPro <vishalsharma8448804514@gmail.com>
2025-06-30 22:12:21 +0530

    Create 1stGroups_Anagrams49.cpp
    Yeh most probably mera khud ka pahla solution hai jo mene khud ki approach se kiya hai

---
+++ Leet-Code/array/1stGroups_Anagrams49.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    
+    unsigned long long getasciiproduct(const string &word){
+        unsigned long long sum = 1;
+        unsigned long long product= 1;
+        for(char ch : word){
+            product *= (int)ch;
+            sum += (int)ch;
+        }
+        return sum + product;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n = strs.size();
+        unordered_map<unsigned long long,vector<string>> mp;
+        vector<vector<string>> result;
+
+        for(int i=0;i<n;i++){
+            unsigned long long pr = getasciiproduct(strs[i]);
+            mp[pr].push_back(strs[i]);
+        }
+        for(auto &it : mp){
+            result.push_back(it.second);
+        }
+        return result;
+    }
+};
+
+//const string &word   Iska matlab: function reference le raha hai par modify nahi karega string ko
+
+// unsigned long long Kya Hota Hai?
+//🔸 long long → 64-bit signed integer
+//Range: −9,223,372,036,854,775,808 to +9,223,372,036,854,775,807
+
+//🔸 unsigned long long → 64-bit unsigned integer
+//Range: 0 to 18,446,744,073,709,551,615 
