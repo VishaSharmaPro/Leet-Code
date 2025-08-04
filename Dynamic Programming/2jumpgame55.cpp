7c3a9dc2a7ff79974a2b1a78dd75cb561028021e
VishaSharmaPro <vishalsharma8448804514@gmail.com>
2025-08-04 11:24:16 +0530

    Create 2jumpgame55.cpp

---
+++ Dynamic Programming/2jumpgame55.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if(n ==1) return true;
+        vector<bool> t(n,false);
+        t[0] = true;
+        for(int i = 0;i<n;i++){
+            if(t[i] == true){
+                for(int j=i+1;j<=i+nums[i];j++){
+                    t[j] = true;
+                    if(j==n-1) return true;
+                }
+            }
+        }
+        return false; 
+    }
+};
