da80fc7b0d401ee038ec151606c5521c18e7e38b
VishaSharmaPro <vishalsharma8448804514@gmail.com>
2025-09-27 17:15:38 +0530

    Update 5uncrossedLines1035.cpp

--- Dynamic Programming/5uncrossedLines1035.cpp
+++ Dynamic Programming/5uncrossedLines1035.cpp
@@ -20,3 +20,20 @@ public:
         return solve(0,0,nums1,nums2);
     }
 };
+
+//Bottom Up
+class Solution {
+public:
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<vector<int>> t(m+1,vector<int>(n+1));
+        for(int i=1;i<m+1;i++){
+            for(int j =1;j<n+1;j++){
+                if(nums1[i-1] == nums2[j-1]) t[i][j] = 1+ t[i-1][j-1];
+                else t[i][j] = max(t[i][j-1],t[i-1][j]);
+            }
+        }
+        return t[m][n];
+    }
+};
