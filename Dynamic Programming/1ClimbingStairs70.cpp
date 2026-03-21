a3b1b38c638cca0ff97ba1ec8451f8ba0d61d4c6
VishaSharmaPro <vishalsharma8448804514@gmail.com>
2026-03-21 16:14:45 +0530

    Update 1ClimbingStairs70.cpp

--- Dynamic Programming/1ClimbingStairs70.cpp
+++ Dynamic Programming/1ClimbingStairs70.cpp
@@ -1,4 +1,4 @@
-//Company Tags  : Google, Amazon, OYO Rooms, Microsoft, Adobe, Flipkart, Siemens(MentorGraphics)
+//Company Tags  : Google, Amazon, OYO Rooms, Microsoft, Adobe, Flipkart, Siemens(MentorGraphics), meta , amd,  zoho, accenture , tiktok , goldman , nvidia, oracle , deloitte, accolite
 class Solution {
 public:
     int t[46];
@@ -13,5 +13,20 @@ public:
     int climbStairs(int n) {
         memset(t,-1,sizeof(t));
         return solve(n);
+    }
+};
+
+// bottom up on 21 march 2026
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 1) return 1;
+        vector<int> t(n,0);
+        t[0] = 1;
+        t[1] = 2;
+        for(int i=2;i<n;i++){
+            t[i] = t[i-1] + t[i-2];
+        }
+        return t[n-1];
     }
 };
