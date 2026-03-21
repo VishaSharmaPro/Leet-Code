dacd403c3f9b2397b98c4ba5bc144844afcffbd3
VishaSharmaPro <vishalsharma8448804514@gmail.com>
2026-03-21 17:16:33 +0530

    Create 6_min-cost-climbing-stairs_LeetCode_746.cpp

---
+++ Dynamic Programming/6_min-cost-climbing-stairs_LeetCode_746.cpp
@@ -0,0 +1,14 @@
+// google amazon microsoft meta bloomberg
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> t(n+1,0);
+        t[0] = cost[0];
+        t[1] = cost[1];
+        for(int i = 2;i<n;i++){
+            t[i] = cost[i] + min(t[i-1],t[i-2]);
+        }
+        return min(t[n-1],t[n-2]);
+    }
+};
