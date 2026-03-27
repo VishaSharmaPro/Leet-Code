f5cc638d9b7f1a1e9a2fd8f57aeb4f82b0685e0f
VishaSharmaPro <vishalsharma8448804514@gmail.com>
2026-03-27 21:47:56 +0530

    Create 17_longest-increasing-path-in-a-matrix_LeetCode_329.cpp

---
+++ Dynamic Programming/17_longest-increasing-path-in-a-matrix_LeetCode_329.cpp
@@ -0,0 +1,38 @@
+// google meta amazon microsoft apple doordash duolingo tiktok uber nutanix adobe phonepe weride
+class Solution {
+public:
+    int ans = 0;
+    int m, n ;
+    int t[201][201];
+    int solve(int i,int j,vector<vector<int>>& matrix){
+        if(t[i][j] != -1) return t[i][j];
+        int left = 0;
+        if(j-1 >= 0 && matrix[i][j-1] > matrix[i][j]){
+            left = 1 + solve(i,j-1,matrix);
+        }
+        int right = 0;
+        if(j+1 < n && matrix[i][j+1] > matrix[i][j]){
+            right = 1 + solve(i,j+1,matrix);
+        }
+        int up = 0;
+        if(i-1 >= 0 && matrix[i-1][j] > matrix[i][j]){
+            up = 1 + solve(i-1,j,matrix);
+        }
+        int down = 0;
+        if(i+1 < m && matrix[i+1][j] > matrix[i][j]){
+            down = 1 + solve(i+1,j,matrix);
+        }
+        return t[i][j] = max({left,right,up,down});
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        m = matrix.size();
+        n = matrix[0].size();
+        memset(t,-1,sizeof(t));
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                ans = max(ans,solve(i,j,matrix));
+            }
+        }
+        return ans+1;
+    }
+};
