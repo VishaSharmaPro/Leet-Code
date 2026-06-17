6af1fcfdfc7d1f78a5a6dd588eaaf309b79a1f7e
VishaSharmaPro <vishalsharma8448804514@gmail.com>
2026-06-17 17:34:53 +0530

    Create 23_next-greater-element-ii_LeetCode_503.cpp

---
+++ Revision Problem/23_next-greater-element-ii_LeetCode_503.cpp
@@ -0,0 +1,19 @@
+// google amazon microsoft meta bloomberg apple flipkart morgantstanley goldman zoho nvidia ituit
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        stack<int> st;
+        
+        vector<int> ans(n,-1);
+        for(int i = 0;i<2*n;i++){
+            int curr = nums[i%n];
+            while(!st.empty() && nums[st.top()] < curr){
+                ans[st.top()] = curr;
+                st.pop();
+            }
+            if(i<n) st.push(i);
+        }
+        return ans;
+    }
+};
