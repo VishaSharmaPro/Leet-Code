71b2bd011d5659473332bc0a962adebf7c38d830
VishaSharmaPro <vishalsharma8448804514@gmail.com>
2026-07-01 20:38:46 +0530

    Update 4_balanced-binary-tree_LeetCode_110.cpp

--- Tree/4_balanced-binary-tree_LeetCode_110.cpp
+++ Tree/4_balanced-binary-tree_LeetCode_110.cpp
@@ -27,3 +27,34 @@ public:
         return flag;
     }
 };
+
+// method 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool flag = true;
+    int dfs(TreeNode* root){
+        if(root == NULL) return 1;
+        int lefth = 1 + dfs(root->left);
+        int righth = 1 + dfs(root->right);
+        if(abs(lefth-righth) >1){
+            flag = false;
+            
+        }
+        return max(lefth,righth);
+    }
+    bool isBalanced(TreeNode* root) {
+        dfs(root);
+        return flag;
+    }
+};
